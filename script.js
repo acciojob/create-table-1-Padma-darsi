fb6552e70a86388f1c51285e8f97e5749da65bd8
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-01-02 15:21:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let samtab=document.getElementById("sampleTable");
+    samtab.addEventListener("click",insert_Row);
+
 function insert_Row() {
     //Write your code here
-  
-  
+   samtab.innerHTML+=`
+         <tr><td>New Cell1</td> 
+		<td>New Cell2</td></tr> `;
 }
