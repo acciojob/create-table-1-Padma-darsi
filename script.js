1e1767c9d523c90b2ffb5d1d8951c21eaee10873
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-08-07 09:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
-  
+   const table=document.getElementById("sampleTable");
+   table.innerHTML+=`<tr><td>New Cell1</td>
+                         <td>New Cell2</td>
+                          </tr>`;	
   
 }
